6178c7d5134ea60cb2d11fab09d781da4230f208
Sean Aye <hello@seanaye.ca>
2020-05-06 14:35:25 -0400

    add setttings display to node view

--- components/ViewNode.vue
+++ components/ViewNode.vue
@@ -15,21 +15,66 @@
           template(v-for='(elem, i) in nodeInfo')
             tr(v-if='!!elem.data' :key='i')
               td {{ elem.dataName }}
-              td.text-center
+              td.text-end
                 copy-pill(
                   color='accent'
                   text-color='warning'
                   :text='elem.data'
+                ).mr-3
+      v-btn(block @click='showSettings = !showSettings')
+      v-expand-transition
+        v-form(
+          v-if='showSettings'
+          ref='form'
+          v-model='valid'
+          lazy-validation
+          @submit.prevent='updateSettings(nodeID, settings)'
+        )
+          v-container
+            v-row(justify='center')
+              v-col(cols='12' sm='4' md='6' ref='colWidth' align-self='stretch')
+                v-row(justify='center' align='center' style='height: 100%')
+                  div
+                    v-switch(v-model='settings.autopilot' label='Autopilot' inset color='highlight')
+                    v-switch(v-model='settings.grpc' label='GRPC' inset color='highlight')
+                    v-switch(v-model='settings.rest' label='REST' inset color='highlight')
+                    v-switch(v-model='settings.tor' label='Tor' inset color='highlight')
+                    v-switch(v-model='settings.keysend' label='Keysend' inset color='highlight')
+              v-col(cols='12' sm='8' md='6')
+                v-color-picker(
+                  v-if='!!colWidth'
+                  v-model='settings.color'
+                  mode='hexa'
+                  hide-mode-switch
+                  show-swatches
+                  :width='computedWidth'
+                ).mx-auto
+              v-col(cols='12').pb-0
+                v-text-field(
+                  v-model='settings.alias'
+                  outlined label='Node Alias'
+                  color='highlight'
+                  background-color='secondary'
                 )
+              v-col(cols='12').pb-0
+                v-combobox(v-model='settings.whitelist' chips='' label='Whitelist' multiple='' outlined='' color='highlight' background-color='secondary' :rules='[validIP]')
+                  template(v-slot:selection='{ attrs, item, select, selected }')
+                    v-chip(v-bind='attrs' :input-value='selected' close='' @click='select' @click:close='remove(item)')
+                      | {{ item }}
+              v-col(cols='12').pt-0
+                v-btn.px-4.warning--text(block type='submit' color='secondary' large='' :loading='loading' :disabled='!valid')
+                  | Update Settings
+
           
 </template>
 <script lang="ts">
 import { defineComponent, computed, ref, watchEffect } from '@vue/composition-api'
-import useNodeApi from '../compositions/useNodeApi'
+import useNodeApi from '~/compositions/useNodeApi'
 import useNodeControls from '~/compositions/useNodeControls'
 import { Context } from '@nuxt/types'
-import { layoutStore, nodeStore } from '../store'
-import { Settings, NodeStatus } from '../types/api'
+import { layoutStore, nodeStore } from '~/store'
+import { Settings, NodeStatus } from '~/types/api'
+import useFormValidation from '~/compositions/useFormValidation'
 
 export default defineComponent({
   async fetch () {
@@ -61,6 +106,16 @@ export default defineComponent({
 
     const { deleteNode, updateSettings, loading } = useNodeApi(root.$nuxt.context)
     const { canStart, canStop, canDelete} = useNodeControls(node)
+    const { valid, form, validIP } = useFormValidation()
+
+    const colWidth = ref<HTMLBaseElement|null>(null)
+    const computedWidth = computed(() => {
+      if (root.$vuetify.breakpoint.smOnly) {
+        return colWidth.value ? 2 * colWidth.value.clientWidth : 0
+      } else {
+        return colWidth.value ? colWidth.value.clientWidth : 0
+      }
+    })
 
     return {
       nodeID,
@@ -73,7 +128,12 @@ export default defineComponent({
       loading,
       canStart,
       canStop,
-      canDelete
+      canDelete,
+      colWidth,
+      valid,
+      form,
+      validIP,
+      computedWidth
     }
   }
 })
