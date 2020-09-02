e8e525ab95db6b0799ccc8fa9eae12dc06763b11
Graham Krizek <graham@krizek.io>
2020-09-02 16:32:15 -0500

    Work in progress downloading macaroons

--- components/viewnode/DataTable.vue
+++ components/viewnode/DataTable.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
 v-container
+  password-dialog(v-model='showPasswordDialog' @done='handleDownload' :error='error' text='Download Macaroon')
+  v-dialog(v-if='downloadReady' max-width='800')
+    v-card.text-center(style='padding: 20px;')
+      v-card-actions
+          v-btn(type='submit' color='highlight' block).info--text Download Macaroon
   v-simple-table(
     :style='{"background-color": $vuetify.theme.currentTheme.secondary}'
   )
@@ -16,6 +21,13 @@ v-container
               title='tls.cert'
             ).mr-3
               | Download
+          td.text-end(v-else-if='k === "Macaroon"')
+            v-chip(
+              color='accent'
+              text-color='warning'
+              @click='downloadMacaroon'
+            ).mr-3
+              | Download
           td.text-end(v-else)
             copy-pill(
               color='accent'
@@ -24,8 +36,10 @@ v-container
             ).mr-3
 </template>
 <script lang="ts">
-import { defineComponent, computed } from '@vue/composition-api'
+import { defineComponent, computed, ref } from '@vue/composition-api'
 import { Node } from '~/types/apiResponse'
+import useNodeApi from '~/compositions/useNodeApi'
+import crypto from 'crypto-js'
 
 export default defineComponent({
   props: {
@@ -35,21 +49,74 @@ export default defineComponent({
     }
   },
   components: {
-    CopyPill: () => import('~/components/core/CopyPill.vue')
+    CopyPill: () => import('~/components/core/CopyPill.vue'),
+    PasswordDialog: () => import('~/components/PasswordDialog.vue')
   },
-  setup (props) {
+  setup (props, { root }) {
     const nodeInfo = computed(() => ({
       Status: props.node.status,
       'LND Version': props.node.lnd_version,
       'Voltage Version': props.node.volt_version,
       'TLS Cert': props.node.tls_cert,
+      'Macaroon': 'Download',
       'Creation Date': props.node.created,
       'Expiry Date': props.node.expires,
       'API Endpoint': props.node.api_endpoint
     }))
 
+    const { connectNode } = useNodeApi(root.$nuxt.context)
+    const showPasswordDialog = ref(false)
+    const downloadReady = ref(false)
+    const encrypted = ref('')
+    const error = ref('')
+
+    async function downloadMacaroon () {
+      showPasswordDialog.value = true
+      try {
+        const res = await connectNode(props.node.node_id, 'admin')
+        var { macaroon } = res
+        encrypted.value = macaroon
+      } catch (e) {
+        error.value = e.toString()
+      }
+    }
+
+    // @ts-ignore
+    function isBase64 (str) {
+        if (str ==='' || str.trim() ===''){ return false; }
+        try {
+            return btoa(atob(str)) == str;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    async function handleDownload (password: string) {
+      // @ts-ignore
+      try {
+        const decrypted = crypto.AES.decrypt(encrypted.value || '', password).toString(crypto.enc.Base64)
+        const decryptResult = atob(decrypted)
+        if (isBase64(decryptResult)) {
+          //macaroon.value = decryptResult
+          downloadReady.value = true
+          showPasswordDialog.value = false
+        } else {
+          error.value = 'Incorrect password'
+        }
+      } catch (e) {
+        console.error('cipher mismatch, macaroon decryption failed')
+        console.error(e)
+        error.value = e.toString()
+      }
+    }
+
     return {
-      nodeInfo
+      nodeInfo,
+      downloadMacaroon,
+      handleDownload,
+      downloadReady,
+      showPasswordDialog,
+      error
     }
   }
 })
