e90a9d4ee645db11fd72a895b6aa8546a105544b
Graham Krizek <graham@krizek.io>
2020-09-24 10:32:32 -0500

    If the hex color is not correct, fallback to #FFFFFF instead of failing

--- compositions/useFormValidation.ts
+++ compositions/useFormValidation.ts
@@ -49,7 +49,8 @@ export default function useFormValidation () {
       hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
     }
     if (hex.length !== 6) {
-      throw new Error('Invalid HEX color.')
+      console.log('Invalid HEX color.')
+      return '#FFFFFF'
     }
     const r = parseInt(hex.slice(0, 2), 16)
     const g = parseInt(hex.slice(2, 4), 16)
