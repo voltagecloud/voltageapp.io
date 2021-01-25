dc4c0cc75d0c0460c2e2a98d589f1a7343deaadf
Sean aye <hello@seanaye.ca>
2021-01-25 15:14:59 -0500

    remove unused code

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,10 +3,6 @@ import { authStore } from '~/store'
 
 export default ({ $axios }: Context) => {
   $axios.onRequest((config) => {
-    // const auth = store.getters['auth/accessToken']
-    // if (auth) {
-    //   config.headers.common.Authorization = `Bearer ${auth}`
-    // }
     if (authStore.user) {
       const access = authStore.user.getSignInUserSession()?.getAccessToken()?.getJwtToken()
       config.headers.common.Authorization = `Bearer ${access}`
