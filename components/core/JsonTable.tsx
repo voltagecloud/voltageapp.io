cf319f018f86813b60e5874628ff5952dff92ad7
Sean aye <hello@seanaye.ca>
2021-01-30 17:27:36 -0500

    add recursive json data table render component

---
+++ components/core/JsonTable.tsx
@@ -0,0 +1,86 @@
+import { defineComponent, PropType, computed, createElement } from '@vue/composition-api'
+import CopyPill from '~/components/core/CopyPill.vue'
+import { VSimpleTable } from 'vuetify/lib'
+
+const h = createElement
+
+type Primitive = String|Number|Boolean
+type JsonObject = Record<string, Primitive|JsonArray>
+type JsonArray = Array<Primitive|JsonObject>
+type JsonData = Primitive|JsonObject|JsonArray
+
+
+export default defineComponent({
+  name: 'json-table',
+  components: {
+    'v-simple-table': VSimpleTable
+  },
+  props: {
+    data: {
+      type: Function as PropType<() => JsonData>,
+      required: false,
+      //validator: (prop) => {
+      //  let validJson = false
+      //  try {
+      //    const res = JSON.stringify(prop())
+      //    validJson = !!res
+      //  } catch {
+      //    validJson = false
+      //  }
+      //  return validJson
+      //}
+    }
+  },
+  setup: (props, { root }) => {
+    const data = computed(() => props.data ? props.data() : null)
+
+    return () => {
+      const dataType = typeof data.value
+      if (dataType === 'string' || dataType === 'number' || dataType === 'boolean') {
+        return (
+          <CopyPill
+            color="accent"
+            text-color="warning"
+            text={`${data.value}`}
+          />
+        )
+      } else if (data.value === null) {
+        return null
+      } else if (data.value instanceof Array) {
+        return (
+          <v-simple-table
+            style={{'background-color': root.$vuetify.theme.currentTheme.secondary}}
+          >
+            <tbody>
+              {data.value.map((elem, i) => (
+                <tr>
+                  <td>{i}</td>
+                  <td>
+                    <json-table data={() => elem} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </v-simple-table>
+        )
+      } else {
+        return (
+          <v-simple-table
+            style={{'background-color': root.$vuetify.theme.currentTheme.secondary}}
+          >
+            <tbody>
+              {Object.entries(data.value).map(([key, val]) => (
+                <tr>
+                  <td>{key}</td>
+                  <td>
+                    <json-table data={() => val} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </v-simple-table>
+        )
+      }
+    }
+  }
+})
