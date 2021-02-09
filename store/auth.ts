714cd40bafe099e5cfb0b26e95bfe2971253133e
Sean aye <hello@seanaye.ca>
2021-02-09 16:41:51 -0500

    add username getter to auth store

--- store/auth.ts
+++ store/auth.ts
@@ -15,4 +15,8 @@ export default class AuthModule extends VuexModule {
       this.user = user
       this.exp = user?.getSignInUserSession()?.getAccessToken().getExpiration() || 0
     }
+
+    get username () {
+      return this.user ? this.user.getUsername() : ''
+    }
 }
