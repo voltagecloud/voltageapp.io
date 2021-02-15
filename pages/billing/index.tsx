8f62f4c49b87e7071b2ea0fe63a71bd111818263
Sean aye <hello@seanaye.ca>
2021-02-15 16:05:35 -0500

    apply page middleware and update styling

--- pages/billing/index.tsx
+++ pages/billing/index.tsx
@@ -6,6 +6,7 @@ import { VContainer, VRow, VCol, VCard } from 'vuetify/lib'
 const h = createElement
 
 export default defineComponent({
+  middleware: ['loadCognito', 'assertAuthed', 'loadUser'],
   components: {
     VContainer, VRow, VCol, VCard
   },
@@ -29,9 +30,11 @@ export default defineComponent({
 
     return () => <v-container>
       <v-row justify="center">
-        <v-col cols="12" md="10" lg="8">
+        <v-col cols="12" lg="10">
           { state.error && <v-card class="pa-3">{state.error}</v-card> }
-          { state.data  && <JsonTable data={() => state.data as JsonData}/> }
+          { state.data  && <v-card color="info" class="pa-3" >
+            <JsonTable data={() => state.data as JsonData}/>
+          </v-card>}
         </v-col>
       </v-row>
     </v-container>
