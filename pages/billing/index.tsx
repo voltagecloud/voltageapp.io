8370252f12d808620af508846ff21bb322fec5e4
Sean aye <hello@seanaye.ca>
2021-02-15 15:20:33 -0500

    add billing page route

---
+++ pages/billing/index.tsx
@@ -0,0 +1,41 @@
+import { defineComponent, reactive, createElement } from '@vue/composition-api'
+import JsonTable, { JsonData } from '~/components/core/JsonTable'
+import useNodeApi from '~/compositions/useNodeApi'
+import { VContainer, VRow, VCol } from 'vuetify/lib'
+
+const h = createElement
+
+export default defineComponent({
+  components: {
+    VContainer, VRow, VCol
+  },
+  setup: (_, ctx) => {
+
+    const { billing, loading } = useNodeApi(ctx.root.$nuxt.context)
+
+    const state = reactive({
+      data: null as null|JsonData,
+      error: ''
+    })
+
+    billing().then(res => {
+      if (!res) {
+        state.error = 'an error occured fetching billing information'
+        return 
+      }
+      state.data = res.data
+      state.error = ''
+    })
+
+    return () => <v-container>
+      <v-row justify="center">
+        <v-col cols="12" md="10" lg="8">
+          <div>{state.error}</div>
+          { !state.data  || <JsonTable data={() => state.data as JsonData}/> }
+        </v-col>
+      </v-row>
+    </v-container>
+  }
+})
+
+
