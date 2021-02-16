1537bc814e231192cce2761378c0dd6bd8e31a2f
Sean aye <hello@seanaye.ca>
2021-02-16 16:53:39 -0500

    add useFetch composition for future typed api fetching

---
+++ compositions/useFetch.ts
@@ -0,0 +1,46 @@
+import { Auth } from '@aws-amplify/auth'
+import { ref } from '@vue/composition-api'
+
+export default function useFetch <T>(endpoint: string) {
+  const data = ref<T|null>(null)
+  const error = ref<Error|null>(null)
+  const statusCode = ref<number>(0)
+  const loading = ref(false)
+
+  async function dispatch(opts: RequestInit) {
+    const user = await Auth.currentAuthenticatedUser()
+    const accessToken = user.getSignInUserSession().getAccessToken()
+    const jwt = accessToken.getJwtToken()
+
+    const { ...otherOpts, headers } === opts
+    
+    loading.value = true
+    const res = await fetch(`${process.env.apiEndpoint}${endpoint}`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}`,
+        ...(opts?.headers || {})
+      },
+      ...otherOpts
+    })
+
+    try {
+      data.value = await res.json() as T
+    } catch (e) {
+      error.value = e
+    } finally {
+      statusCode.value = res.status
+      loading.value = false
+    }
+  }
+
+
+  return {
+    data,
+    error,
+    statusCode,
+    loading,
+    dispatch
+  }
+}
