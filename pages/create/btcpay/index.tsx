24f9bd6b0dc05d631c092ac1d48f76aa43332e4f
Sean aye <hello@seanaye.ca>
2021-02-19 15:24:34 -0500

    work on btcpay server creation

--- pages/create/btcpay/index.tsx
+++ pages/create/btcpay/index.tsx
@@ -1,15 +1,61 @@
-import { defineComponent, createElement, computed } from '@vue/composition-api'
+import { defineComponent, createElement, computed, reactive } from '@vue/composition-api'
 import { nodeStore } from '~/store'
+import { IDName } from '~/store/api'
 
 const h = createElement
+
 
 export default defineComponent({
   setup: () => {
     const mainnetNodes = computed(() => nodeStore.user?.mainnet_nodes || [])
 
-    return () => <div>
-      
-    </div>
+    const state = reactive<{
+      name: string;
+      selectedNode: IDName|null;
+      createKeys: boolean
+    }>({
+      name: '',
+      selectedNode: null,
+      createKeys: true
+    })
+
+    return () => <v-container>
+      <v-row justify="center" align="start">
+        <v-col cols="12" xl="8">
+          <v-container>
+            <v-card color="info">
+              <v-card-title>
+                <div class="mx-auto">Create BTCPay Server Store</div>
+              </v-card-title>
+              <v-card-actions>
+                <v-form>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field
+                          onInput={(val: string) => {state.name = val}}
+                          value={state.name}
+                          outline
+                          background-color="info"
+                        />
+                      </v-col>
+                      <v-col cols="12">
+                        <v-autocomplete
+                          onInput={(val: IDName) => {state.selectedNode = val }}
+                          value={state.selectedNode.name}
+                          items={mainnetNodes.value}
+                          label="Select a Node"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-form>
+              </v-card-actions>
+            </v-card>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
   }
 })
 
