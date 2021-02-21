d18d2a80964dd7e59c71f37e13b6f11881d38e45
Sean aye <hello@seanaye.ca>
2021-02-20 19:44:28 -0500

    add encrypt and backup macaroon util

---
+++ utils/backupMacaroon.ts
@@ -0,0 +1,17 @@
+import { voltageFetch } from '~/utils/fetchClient'
+import crypto from 'crypto-js'
+
+export default async function backupMacaroon (
+  { macaroonText, macaroonType, nodeId, password }:
+  { macaroonText: string, macaroonType: string; nodeId: string; password: string; }
+) {
+  const encrypted = crypto.AES.encrypt(macaroonText, password).toString()
+  return await voltageFetch('/node/macaroon', {
+    method: 'POST',
+    body: JSON.stringify({
+      nodeId,
+      name: macaroonType,
+      macaroon_data: encrypted
+    })
+  })
+}
