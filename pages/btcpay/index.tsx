6b7a14ff66c036d68c1d45f820a02d77adbf30a0
Sean aye <hello@seanaye.ca>
2021-02-24 11:40:46 -0500

    use new btcpay landing card component

--- pages/btcpay/index.tsx
+++ pages/btcpay/index.tsx
@@ -1,12 +1,13 @@
 import { defineComponent, createElement } from '@vue/composition-api'
-import { VContainer, VRow, VCol, VBtn, VImg, VCard, VCardTitle, VCardActions, VCardText, VProgressCircular } from 'vuetify/lib'
+import { VContainer, VRow, VCol, VCard, VProgressCircular } from 'vuetify/lib'
 import useFetch from '~/compositions/useFetch'
+import BTCPayLandingCard from '~/components/BTCPayLandingCard'
 
 const h = createElement
 
 export default defineComponent({
   components: {
-    VContainer, VCol, VRow, VBtn, VImg, VCard, VCardTitle, VCardActions, VCardText, VProgressCircular
+    VContainer, VCol, VRow, VCard, VProgressCircular
   },
   middleware: ['loadCognito', 'assertAuthed', 'loadUser'],
   setup: (_, { root }) => {
@@ -27,7 +28,7 @@ export default defineComponent({
             </v-col>
           </v-row>
         </v-container>
-      } else if (data.value?.btcpayservers?.length) {
+      } else if (!data.value?.btcpayservers?.length) {
         console.log({ data: data.value })
         return <v-container>
           <v-row>
@@ -41,40 +42,7 @@ export default defineComponent({
           </v-row>
         </v-container>
       } else {
-        return <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-card>
-                <v-card-title>
-                  <div class="text-center">Start Accepting Bitcoin Instantly</div>
-                </v-card-title>
-                <v-card-text>
-                  <div class="text-center">
-                    No setup, no waiting, and no KYC. The easiest way to start accepting Bitcoin payments both on chain and on the Lighting Network.
-                  </div>
-                  <v-img
-                    src={require('~/assets/btcpay-logo.png')}
-                    class="mx-auto"
-                    max-height="250"
-                    contain
-                  />
-                </v-card-text>
-                <v-card-actions class="d-flex flex-column">
-                  <v-btn
-                    large
-                    dark
-                    to="/create/btcpay"
-                    color="highlight"
-                    class="mx-auto my-3"
-                  >
-                    Create Store
-                  </v-btn>
-                  <div class="text-center my-3">Free for 7 days then $7.99/month on a 1 year plan</div>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+        return <BTCPayLandingCard />
       }
     }
   }
