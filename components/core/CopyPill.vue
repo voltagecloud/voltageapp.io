e6bd4676cbfb817636b13146217c466f9f66f909
Sean aye <hello@seanaye.ca>
2021-02-25 22:06:45 -0500

    add hiding to copy pill component

--- components/core/CopyPill.vue
+++ components/core/CopyPill.vue
@@ -7,11 +7,12 @@
   )
     template(v-slot:activator="{ on }")
       v-chip(@click="copyText" v-on="on" v-bind="$attrs")
-        | {{ text }}
+        v-icon(v-if='hide') {{icon}}
+        | {{ renderText }}
     | Copied to clipboard!
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed, ref } from '@vue/composition-api'
 import useClipboard from '~/compositions/useClipboard'
 
 export default defineComponent({
@@ -20,19 +21,31 @@ export default defineComponent({
     text: {
       type: String,
       required: true
+    },
+    hide: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
   setup (props, context) {
     const { isCopied, copy } = useClipboard(1000)
+    const hidden = ref(true)
+    const icon = computed(() => hidden.value ? 'mdi-eye' : 'mdi-eye-off')
 
     function copyText () {
+      hidden.value = !hidden.value
       context.emit('click')
       copy(props.text)
     }
 
+    const renderText = computed(() => hidden.value && props.hide ? '•••••••' : props.text)
+
     return {
       isCopied,
-      copyText
+      copyText,
+      renderText,
+      icon
     }
   }
 })
