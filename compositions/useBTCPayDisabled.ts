89757ec71d49f380a335d8f838c01a8acef897e3
Sean aye <hello@seanaye.ca>
2021-02-27 20:30:49 -0500

    change available logic

--- compositions/useBTCPayDisabled.ts
+++ compositions/useBTCPayDisabled.ts
@@ -8,13 +8,20 @@ export default function useBTCPayDisabled () {
   const { dispatch: userDispatch, data: userData, loading: userLoading } = useFetch<any>('/user')
   userDispatch({ method: 'GET' })
 
-  const alreadyHasIntance = computed(() => !!data.value?.btcpayservers?.find((server: any) => server.purchase_status !== 'trial'))
-  const hasAvailable = computed(() => !!userData.value?.available_btcpayservers)
+  const alreadyHasIntance = computed(() => !!data.value?.btcpayservers?.find((server: any) =>
+    server.purchase_status !== 'trial'
+  ))
+
+  const hasAvailable = computed(() => !!userData.value?.available_btcpayservers
+    || userData.value.btcpayserver_trial
+  )
 
   const mergedLoading = computed(() => loading.value || userLoading.value || !data.value)
 
   const canPurchase = computed(() => !mergedLoading.value && !hasAvailable.value && !alreadyHasIntance.value)
-  const canCreate = computed(() => !mergedLoading.value && hasAvailable.value || !alreadyHasIntance.value)
+  const canCreate = computed(() => !mergedLoading.value && hasAvailable.value && !alreadyHasIntance.value)
+
+  console.log({ data, userData })
 
   return {
     loading: mergedLoading,
