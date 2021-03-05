f2ff6693950cc8d866712bdf9f0b83556d2f42c7
Sean aye <hello@seanaye.ca>
2021-03-05 11:01:40 -0500

    use ensure base64 and add logging

--- compositions/useBackupMacaroon.ts
+++ compositions/useBackupMacaroon.ts
@@ -2,6 +2,7 @@ import {
   bakeMacaroon,
   backupMacaroon,
   MacaroonType,
+  ensureBase64
 } from "~/utils/bakeMacaroon";
 import { ref } from "@vue/composition-api";
 import { macaroonStore } from "~/store";
@@ -27,18 +28,30 @@ export default function useBackupMacaroon() {
     loading.value = true;
     try {
       const res = await bakeMacaroon({ endpoint, macaroonType, macaroonHex });
+      console.log({ res })
       let { macaroon } = (await res.json()) as { macaroon: string };
+      console.log({ macaroon })
+      macaroon = ensureBase64(macaroon)
+      console.log({ macaroon })
       const { encrypted } = await backupMacaroon({
         macaroon,
         macaroonType,
         nodeId,
         password,
       });
+      console.log({ encrypted })
       macaroonStore.MACAROON({
         nodeId,
         macaroon: encrypted,
         type: macaroonType,
       });
+
+      // TEMP! delete me after
+      const { macaroon: decrypted } = macaroonStore.macaroonState({
+        nodeId,
+        type: macaroonType
+      })
+      console.assert(decrypted === macaroon)
     } catch (e) {
       console.error(e);
       error.value = e.message;
