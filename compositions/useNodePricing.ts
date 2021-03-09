803a3512f28078ff677a7ef30b0b41d0c4a00580
Sean aye <hello@seanaye.ca>
2021-03-09 18:42:52 -0500

    create use node pricing composition

---
+++ compositions/useNodePricing.ts
@@ -0,0 +1,21 @@
+import { computed, ref } from '@vue/composition-api'
+import { standardPlans, litePlans, Plan, Subscription } from '~/utils/voltageProducts'
+
+export default function useNodePricing() {
+  const billingCycle = ref(Plan.monthly)
+
+  const yearlyBilling = computed({
+    get: () => billingCycle.value === Plan.yearly,
+    set: (v: boolean) => billingCycle.value = v ? Plan.yearly : Plan.monthly
+  })
+
+  const litePlan = computed(() => litePlans.find(e => e.plan === billingCycle.value) as Subscription)
+  const standardPlan = computed(() => standardPlans.find(e => e.plan === billingCycle.value) as Subscription)
+
+  return {
+    billingCycle,
+    litePlan,
+    standardPlan,
+    yearlyBilling
+  }
+}
