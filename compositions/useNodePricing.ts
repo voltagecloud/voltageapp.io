a0b0ba517717ec2c449e68b2caa916fdc9c35659
Sean aye <hello@seanaye.ca>
2021-03-11 15:14:04 -0500

    update node priving interface

--- compositions/useNodePricing.ts
+++ compositions/useNodePricing.ts
@@ -1,16 +1,16 @@
 import { computed, ref } from '@vue/composition-api'
-import { standardPlans, litePlans, Plan, Subscription } from '~/utils/voltageProducts'
+import { standardPlans, litePlans, Plan } from '~/utils/voltageProducts'
 
 export default function useNodePricing() {
-  const billingCycle = ref(Plan.monthly)
+  const billingCycle = ref<Plan>(Plan.monthly)
 
   const yearlyBilling = computed({
     get: () => billingCycle.value === Plan.yearly,
     set: (v: boolean) => billingCycle.value = v ? Plan.yearly : Plan.monthly
   })
 
-  const litePlan = computed(() => litePlans.find(e => e.plan === billingCycle.value) as Subscription)
-  const standardPlan = computed(() => standardPlans.find(e => e.plan === billingCycle.value) as Subscription)
+  const litePlan = computed(() => litePlans.find(e => e.plan === billingCycle.value))
+  const standardPlan = computed(() => standardPlans.find(e => e.plan === billingCycle.value))
 
   return {
     billingCycle,
