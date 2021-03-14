8ca7501a180c5006b00359a7ac328c89482c1f3b
Sean aye <hello@seanaye.ca>
2021-03-14 15:03:37 -0400

    conditionally render NodeCreationFinalize

--- pages/create/lnd/index.tsx
+++ pages/create/lnd/index.tsx
@@ -1,16 +1,26 @@
-import { defineComponent, ref } from '@vue/composition-api'
-import { useRoute } from '@nuxtjs/composition-api'
-import NodeCreationSetup from '~/components/NodeCreationSetup'
+import { defineComponent, ref } from "@vue/composition-api";
+import { useRoute } from "@nuxtjs/composition-api";
+import NodeCreationSetup from "~/components/NodeCreationSetup";
+import NodeCreationFinalize from "~/components/NodeCreationFinalize";
 
 export default defineComponent({
   setup: () => {
-    const route = useRoute()
+    const route = useRoute();
 
-    const currentStep = ref(route.value.query.session_id ? 1 : 0)
+    const currentStep = ref(route.value.query.session_id ? 1 : 0);
 
-    return () => <div>
-      {currentStep.value === 0 ? <NodeCreationSetup btcPayServerToggle callbackPath='/create/lnd' /> : ''}
-    </div>
-  }
-})
-
+    return () => (
+      <div>
+        {currentStep.value === 0 ? (
+          <NodeCreationSetup
+            btcPayServerToggle
+            callbackPath="/create/lnd"
+            onNext={() => (currentStep.value = 1)}
+          />
+        ) : (
+          <NodeCreationFinalize />
+        )}
+      </div>
+    );
+  },
+});
