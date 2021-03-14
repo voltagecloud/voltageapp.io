ef6494dcc19f86c68642502b56e6e3e176f2c36a
Sean aye <hello@seanaye.ca>
2021-03-14 15:03:49 -0400

    add node creation finalize

---
+++ components/NodeCreationFinalize.tsx
@@ -0,0 +1,72 @@
+import { defineComponent, computed, ref } from "@vue/composition-api";
+import { VCard, VContainer, VCol, VTextField, VIcon } from "vuetify/lib";
+import { createStore } from "~/store";
+import { useConfirmPassword } from "~/compositions/useConfirmPassword";
+
+export default defineComponent({
+  setup: () => {
+    const nodeName = computed({
+      get: () => createStore.nodeName,
+      set: (v: string) => createStore.NODE_NAME(v),
+    });
+
+    const nameError = ref("");
+
+    const {
+      password,
+      confirmPassword,
+      handlePassword,
+      handleConfirm,
+      inputType,
+      showPassword,
+      validate,
+    } = useConfirmPassword();
+
+    return () => (
+      <VContainer>
+        <div class="d-flex flex-row justify-center">
+          <VCol cols="12" md="10" lg="8">
+            <VCard color="info">
+              <div class="text-h5 pa-3">Node Name</div>
+              <div class="mx-12">
+                <VTextField
+                  outlined
+                  background-color="secondary"
+                  value={nodeName.value}
+                  onInput={(v: string) => (nodeName.value = v)}
+                />
+                {nameError.value && (
+                  <div class="text--error">{nameError.value}</div>
+                )}
+              </div>
+              <div class="text-h5 pa-3">Node Password</div>
+              <div class="mx-12 d-flex flex-column">
+                <VTextField
+                  outlined
+                  background-color="secondary"
+                  value={password.value}
+                  onInput={handlePassword}
+                  type={inputType.value}
+                >
+                  <VIcon
+                    slot="append"
+                    onClick={() => (showPassword.value = !showPassword.value)}
+                  >
+                    {showPassword.value ? "mdi-eye-off" : "mdi-eye"}
+                  </VIcon>
+                </VTextField>
+                <VTextField
+                  outlined
+                  background-color="secondary"
+                  value={confirmPassword.value}
+                  onInput={handleConfirm}
+                  type={inputType.value}
+                />
+              </div>
+            </VCard>
+          </VCol>
+        </div>
+      </VContainer>
+    );
+  },
+});
