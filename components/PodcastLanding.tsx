6032c35fb718ce25c948a4a9ffff94e7ec35bbb3
Sean aye <hello@seanaye.ca>
2021-03-16 10:27:48 -0400

    update podcast landing page to reflect Breez changes

--- components/PodcastLanding.tsx
+++ components/PodcastLanding.tsx
@@ -1,24 +1,48 @@
-import { defineComponent, ref } from "@vue/composition-api";
+import { defineComponent, computed } from "@vue/composition-api";
 import { VCard, VContainer, VRow, VCol, VSwitch, VCheckbox } from "vuetify/lib";
-import useNodePricing from "~/compositions/useNodePricing";
-import useCart from "~/compositions/useCart";
 import useStripeCheckout from "~/compositions/useStripeCheckout";
-import type { Subscription, Plan, Product } from "~/utils/voltageProducts";
+import { createStore } from "~/store";
+import {
+  Subscription,
+  Plan,
+  Product,
+  podcastPlans,
+} from "~/utils/voltageProducts";
 
 export default defineComponent({
   setup: () => {
-    const { litePlan, standardPlan, yearlyBilling } = useNodePricing();
+    const cart = computed(() => createStore.cart);
 
-    const { cart, planState } = useCart();
+    const planState = computed({
+      get: () => createStore.planState as Subscription<Plan, Product.podcast>,
+      set: (v: Subscription<Plan, Product.podcast>) =>
+        createStore.PLAN_STATE(v),
+    });
 
     const { stripeCheckout } = useStripeCheckout(cart);
 
-    const includeBreeze = ref(false);
-
-    async function checkout(plan?: Subscription<Plan, Product>) {
-      if (!plan) return
+    async function checkout(plan?: Subscription<Plan, Product.podcast>) {
+      if (!plan) return;
       planState.value = Object.assign({}, plan);
-      await stripeCheckout('/create/lnd');
+      // serialize store for retrieval after redirect
+      createStore.SERIALIZE();
+      await stripeCheckout("/create/lnd");
+    }
+
+    function renderPlan(sub: Subscription<Plan, Product.podcast>) {
+      return (
+        <VCol cols="12" md="6" lg="4">
+          <VCard onClick={() => checkout(sub)} class="pa-6" color="secondary">
+            <div class="d-flex flex-column">
+              <div class="text-h5">
+                {sub.plan === Plan.monthly && "Monthly"}
+                {sub.plan === Plan.yearly && "Yearly"}
+              </div>
+              <div class="text-h6">${sub.cost}/mo</div>
+            </div>
+          </VCard>
+        </VCol>
+      );
     }
 
     return () => (
@@ -31,59 +55,8 @@ export default defineComponent({
                 Let your listeners stream sats while they listen to your podcast
               </div>
               <div class="my-6 text-h4">Create your node to get started</div>
-              <div class="d-flex flex-row justify-center align-center">
-                <div class="mx-4">Montly</div>
-                <VSwitch
-                  inset
-                  value={yearlyBilling.value}
-                  onChange={(v: boolean) => {
-                    yearlyBilling.value = v;
-                  }}
-                />
-                <div class="mx-2">Yearly</div>
-              </div>
-              <div class="d-flex flex-row justify-center align-center">
-                <VCheckbox
-                  value={includeBreeze.value}
-                  onChecked={(v: boolean) => {
-                    includeBreeze.value = v;
-                  }}
-                >
-                  <div slot="label" class="d-flex flex-row align-center">
-                    Include a channel from
-                    <img
-                      src={require("~/assets/breez.png")}
-                      alt="Breeze Logo"
-                      width="80"
-                    />
-                  </div>
-                </VCheckbox>
-              </div>
               <div class="d-flex flex-row justify-space-around flex-wrap">
-                <VCol cols="12" md="6" lg="4">
-                  <VCard
-                    onClick={() => checkout(litePlan.value)}
-                    class="pa-6"
-                    color="secondary"
-                  >
-                    <div class="d-flex flex-column">
-                      <div class="text-h5">Basic</div>
-                      <div class="text-h6">${litePlan.value?.cost || ''}/mo</div>
-                    </div>
-                  </VCard>
-                </VCol>
-                <VCol cols="12" md="6" lg="4">
-                  <VCard
-                    onClick={() => checkout(standardPlan.value)}
-                    class="pa-6"
-                    color="secondary"
-                  >
-                    <div class="d-flex flex-column">
-                      <div class="text-h5">Premium</div>
-                      <div class="text-h6">${standardPlan.value?.cost || ''}/mo</div>
-                    </div>
-                  </VCard>
-                </VCol>
+                {podcastPlans.map(sub => renderPlan(sub))}
               </div>
             </VCol>
           </VRow>
