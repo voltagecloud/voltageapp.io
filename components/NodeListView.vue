3d1f9cdb0d9a879789d70f3e313173f12fdf64ef
Sean aye <hello@seanaye.ca>
2021-03-17 12:37:38 -0400

    add HUD of running nodes

--- components/NodeListView.vue
+++ components/NodeListView.vue
@@ -1,6 +1,19 @@
 <template>
-  <VirtualScroll :items="nodes" :height="120" :renderAhead="1">
+  <VirtualScroll :items="nodes" :height="120" :renderAhead="1" :extra="1">
     <template v-slot:default="{ items }">
+      <div
+        class="d-flex flex-row justify-space-between"
+        style="height: 120px; background-color: white"
+      >
+        <v-col cols="12" md="10" lg="8" class="mx-auto">
+          <div class="d-flex flex-row align-center justify-space-between" style="height: 100%;">
+            <div class="text-h5">{{ numRunning }} Nodes Running</div>
+            <v-btn dark color="highlight" @click="$router.push('/create/lnd')"
+              >Launch New</v-btn
+            >
+          </div>
+        </v-col>
+      </div>
       <div v-for="(node, i) in items" :key="`${node.node_id}_${i}`">
         <v-col cols="12" md="10" lg="8" class="mx-auto">
           <node-controls :nodeID="node.node_id" />
@@ -10,18 +23,21 @@
   </VirtualScroll>
 </template>
 <script lang="ts">
-import { defineComponent, computed } from '@vue/composition-api'
-import { nodeStore } from '~/store'
+import { defineComponent, computed } from "@vue/composition-api";
+import { nodeStore } from "~/store";
 
 export default defineComponent({
   components: {
-    VirtualScroll: () => import('~/components/core/VirtualScroll.vue'),
-    NodeControls: () => import('~/components/viewnode/NodeControls.vue')
+    VirtualScroll: () => import("~/components/core/VirtualScroll.vue"),
+    NodeControls: () => import("~/components/viewnode/NodeControls.vue"),
   },
   setup: () => {
     return {
-      nodes: computed(() => nodeStore.IDNames)
-    }
-  }
-})
+      nodes: computed(() => nodeStore.IDNames),
+      numRunning: computed(
+        () => nodeStore.nodes.filter((node) => node.status === "running").length
+      ),
+    };
+  },
+});
 </script>
