af5dd16bbe8f69af63b5826604dddbf4ae5be703
Sean aye <hello@seanaye.ca>
2021-03-22 11:02:12 -0400

    remove redirect

--- middleware/captureReferral.ts
+++ middleware/captureReferral.ts
@@ -1,7 +1,7 @@
 import { Context, Middleware } from "@nuxt/types";
 import { createStore, nodeStore } from "~/store";
 
-const captureReferral: Middleware = ({ route, redirect }: Context) => {
+const captureReferral: Middleware = ({ route }: Context) => {
   const code: string | null =
     (route.query.podcast_id as string | null) ||
     localStorage.getItem("podcast_id") ||
@@ -9,8 +9,6 @@ const captureReferral: Middleware = ({ route, redirect }: Context) => {
   if (code && !createStore.referralCode) {
     localStorage.setItem("podcast_id", code);
     createStore.REFERRAL(code);
-    // if user doesnt have nodes redirect
-    !nodeStore.nodes.length && route.path !== '/podcast' && redirect('/podcast')
   }
 };
 
