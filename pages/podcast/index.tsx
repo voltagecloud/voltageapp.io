169dd9c2820f99f66dcb05c0a89fbe7948a8c957
Sean aye <hello@seanaye.ca>
2021-03-24 19:13:17 -0400

    Add container padding

--- pages/podcast/index.tsx
+++ pages/podcast/index.tsx
@@ -1,10 +1,17 @@
-import { defineComponent } from '@vue/composition-api'
-import PodcastLanding from '~/components/PodcastLanding'
+import { defineComponent } from "@vue/composition-api";
+import { VContainer, VRow, VCol } from "vuetify/lib";
+import PodcastLanding from "~/components/PodcastLanding";
 
 export default defineComponent({
   setup: () => {
-
-    return () => <PodcastLanding />
-  }
-})
-
+    return () => (
+      <VContainer>
+        <VRow justify="center" align="center" no-gutters="no-gutters">
+          <VCol cols="12" md="10" lg="8">
+            <PodcastLanding />
+          </VCol>
+        </VRow>
+      </VContainer>
+    );
+  },
+});
