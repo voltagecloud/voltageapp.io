9a47c6ed7b4de70ea1ac6ccc9860e4267d99ee1c
Sean aye <hello@seanaye.ca>
2021-03-24 15:46:35 -0400

    style checkout card like purchase page

--- components/PrepayPurchaseCard.tsx
+++ components/PrepayPurchaseCard.tsx
@@ -1,8 +1,9 @@
 import { defineComponent, computed } from "@vue/composition-api";
-import { VCard, VList, VListItem, VBtn } from "vuetify/lib";
+import { VCard, VBtn, VDivider, VSlider, VContainer } from "vuetify/lib";
 import { createStore } from "~/store";
 import useStripeCheckout from "~/compositions/useStripeCheckout";
 import useBitcoinCheckout from "~/compositions/useBitcoinCheckout";
+import { Product } from "~/utils/voltageProducts";
 
 enum Checkout {
   bitcoin = "bitcoin",
@@ -18,6 +19,10 @@ export default defineComponent({
   },
   setup: (props) => {
     const cart = computed(() => createStore.cart);
+    const planQty = computed({
+      get: () => createStore.planQty,
+      set: (v: number) => createStore.PLAN_QTY(v)
+    })
 
     const { stripeCheckout, loading, error } = useStripeCheckout(cart);
     const {
@@ -35,23 +40,51 @@ export default defineComponent({
       }
     }
 
+    const planState = computed(() => createStore.planState);
+
     return () => (
       <VCard>
+        <VContainer>
         <div class="d-flex flex-column justify-center">
-          <VList>
-            {cart.value.items.map((item) => (
-              <VListItem>
-                <div class="d-flex flex-row justify-space-around">
-                  <div>{item.plan}</div>
-                  <div>{item.quantity}</div>
-                </div>
-              </VListItem>
-            ))}
-          </VList>
+          <div class="text-h5 text-center">Order Details</div>
+          <div class="d-flex justify-space-between my-3">
+            <div>{planState.value.name}</div>
+            <div>${planState.value.cost}/mo</div>
+          </div>
+          <div class="d-flex flex-column justify-space-between my-3">
+            <div class="text-h6">Description</div>
+            <div class="text-caption">{planState.value.desc}</div>
+          </div>
+          {planState.value.nodeType !== Product.btcPay && (
+            <div>
+              <div class="my-3">
+                <div>Node Quantity:</div>
+                <VSlider
+                  class="mt-10"
+                  onChange={(val: number) => {
+                    planQty.value = val;
+                  }}
+                  value={planQty.value}
+                  thumb-label="always"
+                  track-color="highlight"
+                  color="primary"
+                  max="25"
+                  min="1"
+                />
+              </div>
+              <VDivider />
+            </div>
+          )}
+          <div class="d-flex justify-space-between">
+            <div>Total:</div>
+            <div class="font-weight-bold">${cart.value.totalPrice}</div>
+          </div>
+          <VDivider />
           <VBtn
             block
             onClick={() => handleCheckout(Checkout.stripe)}
             loading={loading.value}
+            class="my-2"
           >
             Checkout with Card
           </VBtn>
@@ -59,11 +92,13 @@ export default defineComponent({
             block
             onClick={() => handleCheckout(Checkout.bitcoin)}
             loading={btcLoading.value}
+            class="my-2"
           >
             Checkout with Bitcoin
           </VBtn>
           <div class="text--error">{error.value || btcError.value}</div>
         </div>
+          </VContainer>
       </VCard>
     );
   },
