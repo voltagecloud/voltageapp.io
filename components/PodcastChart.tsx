4ae4a38591b904831a10bb6b8e4d15181eec073f
Sean aye <hello@seanaye.ca>
2021-04-01 17:23:37 -0400

    render bar groups for boosted and streamed sats

--- components/PodcastChart.tsx
+++ components/PodcastChart.tsx
@@ -3,29 +3,33 @@ import Chart from "chart.js";
 
 export default defineComponent({
   props: {
-    podcastData: {
-      type: Array as PropType<{ x: string; y: number }[]>,
+    chartData: {
+      type: Array as PropType<{ x: string; boostY: number; streamY: number }[]>,
       required: true,
     },
   },
   setup: (props, { refs }) => {
     function renderChart() {
-      const chartData = props.podcastData;
       const canvas = refs.chart;
-      if (!chartData || !canvas) {
-        return;
-      }
       new Chart(canvas as HTMLCanvasElement, {
         type: "bar",
         data: {
-          labels: chartData.map((e) => e.x),
+          // both boostData and streamData should have exact same length
+          labels: props.chartData.map(e => e.x),
           datasets: [
             {
-              label: "Sats Streamed",
-              data: chartData,
+              label: "Streamed Sats",
+              data: props.chartData.map(e => e.streamY),
               barPercentage: 0.5,
               minBarLength: 2,
               backgroundColor: '#343851'
+            },
+            {
+              label: "Boosted Sats",
+              data: props.chartData.map(e => e.boostY),
+              barPercentage: 0.5,
+              minBarLength: 2,
+              backgroundColor: '#EF820D'
             },
           ],
         },
