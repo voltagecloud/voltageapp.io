5337aac8df44eb8c4a65527869e73e40efeb5a48
Sean aye <hello@seanaye.ca>
2021-04-07 12:16:48 -0400

    rework auth assertion logic

--- middleware/assertAuthed.ts
+++ middleware/assertAuthed.ts
@@ -2,16 +2,22 @@ import { Context, Middleware } from '@nuxt/types'
 import { authStore } from '~/store'
 
 const assertAuthed: Middleware = ({ redirect, route }: Context) => {
-  // bypass auth check for /podcast route
-  if (route.path === '/podcast') return
+  // capture redirect path
   if (!authStore.user) {
     authStore.REDIRECT(route.path)
-    return redirect('/login')
-  } else if (authStore.user && authStore.redirect) {
+  } 
+
+
+  // bypass auth check for /podcast route
+  if (route.path === '/podcast') return
+
+  if (authStore.user && authStore.redirect) {
     const to = authStore.redirect
     authStore.REDIRECT('')
     return redirect(to)
   }
+
+  return redirect('/login')
 }
 
 export default assertAuthed
