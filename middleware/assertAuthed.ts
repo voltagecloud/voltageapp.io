95de515d2d9735b6f5a10139e1aa3cee5174db91
Sean aye <hello@seanaye.ca>
2021-04-07 11:56:39 -0400

    add podcast route fallthrough for middleware

--- middleware/assertAuthed.ts
+++ middleware/assertAuthed.ts
@@ -2,6 +2,8 @@ import { Context, Middleware } from '@nuxt/types'
 import { authStore } from '~/store'
 
 const assertAuthed: Middleware = ({ redirect, route }: Context) => {
+  // bypass auth check for /podcast route
+  if (route.path === '/podcast') return
   if (!authStore.user) {
     authStore.REDIRECT(route.path)
     return redirect('/login')
