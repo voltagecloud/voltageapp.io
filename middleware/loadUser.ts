5ae3eaa270cc238574973231e042e5c7290cb407
Sean aye <hello@seanaye.ca>
2021-04-07 11:57:13 -0400

    skip store hydration if user isnt signed in

--- middleware/loadUser.ts
+++ middleware/loadUser.ts
@@ -1,8 +1,11 @@
 import { Context, Middleware } from '@nuxt/types'
-import { nodeStore } from '~/store'
+import { nodeStore, authStore } from '~/store'
 import type { User, Node } from '~/types/apiResponse'
 
 const loadUser: Middleware = async ({ $axios }: Context) => {
+  // skip if user not signed in
+  if (!authStore.user) return
+  // skip if data already exists
   if (!!nodeStore.user) return
   const user = await $axios.get<User>('/user')
   const nodes = await $axios.get<{nodes: Node[]}>('/node')
