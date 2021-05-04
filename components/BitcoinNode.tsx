e0fa09efbbcd9b2e88eff607ab6108c2a501fbd1
Sean aye <hello@seanaye.ca>
2021-05-04 12:46:58 -0400

    dont render all fields

--- components/BitcoinNode.tsx
+++ components/BitcoinNode.tsx
@@ -1,28 +1,43 @@
-import { defineComponent, computed } from '@vue/composition-api'
-import JsonTable, { JsonData } from '~/components/core/JsonTable'
-import { useRoute } from '@nuxtjs/composition-api'
-import { nodeStore } from '~/store'
-import { VContainer } from 'vuetify/lib'
-import NodeControls from '~/components/viewnode/NodeControls.vue'
+import { defineComponent, computed } from "@vue/composition-api";
+import JsonTable, { JsonData } from "~/components/core/JsonTable";
+import { useRoute } from "@nuxtjs/composition-api";
+import { nodeStore } from "~/store";
+import { VContainer } from "vuetify/lib";
+import NodeControls from "~/components/viewnode/NodeControls.vue";
+import type { BitcoindNode } from "~/types/apiResponse";
 
 export default defineComponent({
   setup: () => {
-    const route = useRoute()
+    const route = useRoute();
 
-    const data = computed(() => nodeStore.nodeData(route.value.params.id))
+    const data = computed(
+      () => nodeStore.nodeData(route.value.params.id) as BitcoindNode
+    );
 
-    return () => <VContainer>
-      <NodeControls nodeID={route.value.params.id} 
-        {
-          ...{
+    const payload = computed(() => ({
+      Status: data.value?.status,
+      "Bitcoind Version": data.value.bitcoind_version,
+      "Voltage Version": data.value.volt_version,
+      "Creation Date": data.value.created,
+      "Expiry Date": data.value.expires,
+      "Api Endpoint": data.value.api_endpoint,
+      Username: data.value.username,
+      Password: data.value.password,
+    }));
+
+    return () => (
+      <VContainer>
+        <NodeControls
+          nodeID={route.value.params.id}
+          {...{
             scopedSlots: {
-              "append-content": () => <JsonTable data={() => data.value as unknown as JsonData} />
-            }
-          }
-        }
-      >
-      </NodeControls>
-    </VContainer>
-  }
-})
-
+              "append-content": () => (
+                <JsonTable data={() => (payload.value as unknown) as JsonData} />
+              ),
+            },
+          }}
+        ></NodeControls>
+      </VContainer>
+    );
+  },
+});
