6441efd00e2249e5ca652d756a843ca334268b9b
Sean aye <hello@seanaye.ca>
2021-05-06 15:38:04 -0400

    add logging

--- components/SlottedGRPC.tsx
+++ components/SlottedGRPC.tsx
@@ -14,6 +14,7 @@ export default defineComponent({
   },
   setup: (props) => {
     return () => {
+      console.log({ payload: props.node })
       if (props.node.status !== "running") {
         return (
           <div class="ma-3 text-center">
