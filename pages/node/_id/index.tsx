e6e30218dc71b12df189cf4baaed78ac119d37f3
Sean aye <hello@seanaye.ca>
2021-05-05 23:04:55 -0400

    fetch nodes on component render

--- pages/node/_id/index.tsx
+++ pages/node/_id/index.tsx
@@ -1,24 +1,36 @@
-import { defineComponent, computed } from '@vue/composition-api'
-import { nodeStore } from '~/store'
-import { useRoute } from '@nuxtjs/composition-api'
-import { NodeSoftware } from '~/types/apiResponse'
-import LndNode from '~/components/LndNode.vue'
-import BitcoinNode from '~/components/BitcoinNode'
+import { defineComponent, computed } from "@vue/composition-api";
+import { nodeStore } from "~/store";
+import { useRoute } from "@nuxtjs/composition-api";
+import { NodeSoftware } from "~/types/apiResponse";
+import LndNode from "~/components/LndNode.vue";
+import BitcoinNode from "~/components/BitcoinNode";
+import { voltageFetch } from "~/utils/fetchClient";
+import type { Node } from "~/types/apiResponse";
 
 export default defineComponent({
   setup: () => {
-    const route = useRoute()
+    voltageFetch("/node", { method: "GET" }).then((res: any) =>
+      res.json().then(({ nodes }: { nodes: Node[] }) => {
+        nodeStore.HYDRATE_USER({ nodes });
+      })
+    );
 
-    const nodeSoftware = computed(() => nodeStore.nodeData(route.value.params.id)?.node_type)
+    const route = useRoute();
+
+    const nodeSoftware = computed(
+      () => nodeStore.nodeData(route.value.params.id)?.node_type
+    );
 
     return () => {
-      console.log({ nodeSoftware })
+      console.log({ nodeSoftware });
       if (nodeSoftware.value === NodeSoftware.lnd) {
-        return <LndNode />
-      } else if (nodeSoftware.value === NodeSoftware.bitcoind || nodeSoftware.value === NodeSoftware.btcd) {
-        return <BitcoinNode />
+        return <LndNode />;
+      } else if (
+        nodeSoftware.value === NodeSoftware.bitcoind ||
+        nodeSoftware.value === NodeSoftware.btcd
+      ) {
+        return <BitcoinNode />;
       }
-    }
-  }
-})
-
+    };
+  },
+});
